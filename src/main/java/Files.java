d52bba6e4232ec2feae4514141de06322f67d785
Brian Pontarelli <brian@inversoft.com>
2014-02-25 09:33:14 -0700

    Fixed up for easier testing

--- src/main/java/Files.java
+++ src/main/java/Files.java
@@ -3,4 +3,8 @@ import java.io.File;
 public class Files {
   public void prettyPrint(File f, boolean includeSubDirs, final int tabs) {
   }
+
+  public static void main(String... args) {
+    // Fill-out with test
+  }
 }
--- src/main/java/Sums.java
+++ src/main/java/Sums.java
@@ -1,7 +1,10 @@
-import java.util.Arrays;
-
 public class Sums {
   // findSums(5, [1, 4, 3, 5, 2, 0] = 3
   public int findSums(int value, int[] nums) {
+    return 0;
+  }
+
+  public static void main(String... args) {
+    // Fill-out with test
   }
 }
