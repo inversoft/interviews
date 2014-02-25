e85afdeae4372a7d15f08ec2505ce5c8e5c00bf1
Brian Pontarelli <brian@inversoft.com>
2014-02-25 09:24:19 -0700

    Initial commit

---
+++ src/main/java/Files.java
@@ -0,0 +1,6 @@
+import java.io.File;
+
+public class Files {
+  public void prettyPrint(File f, boolean includeSubDirs, final int tabs) {
+  }
+}
---
+++ src/main/java/Sums.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+
+public class Sums {
+  // findSums(5, [1, 4, 3, 5, 2, 0] = 3
+  public int findSums(int value, int[] nums) {
+  }
+}
